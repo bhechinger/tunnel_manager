f7cae01b5817e2c12064289e1636a531b718109d
bhechinger <wonko@4amlunch.net>
2022-11-11 12:50:39 +0000

    started getting tracing setup. Implemented Into for Users -> UserData mapping

--- src/models/users.rs
+++ src/models/users.rs
@@ -1,5 +1,6 @@
 use sqlx::postgres::PgPool;
 use tonic::Status;
+use tracing::instrument;
 
 use crate::api::UserData;
 use crate::models::helpers::sql_err_to_grpc_error;
@@ -19,37 +20,59 @@ impl Default for Users {
     }
 }
 
+impl Into<UserData> for Users {
+    fn into(self) -> UserData {
+        UserData {
+            id: self.id,
+            email: self.email.clone(),
+        }
+    }
+}
+
+impl Into<UserData> for &Users {
+    fn into(self) -> UserData {
+        UserData {
+            id: self.id,
+            email: self.email.clone(),
+        }
+    }
+}
+
 impl Users {
+    #[instrument]
     pub async fn all(pool: &PgPool) -> Result<Vec<UserData>, Status> {
         match sqlx::query_as!(Users, "SELECT id, email FROM users ORDER by id")
             .fetch_all(pool)
             .await
         {
-            Ok(users) => Ok(users.iter().map(|t| t.into_response()).collect()),
+            Ok(users) => Ok(users.iter().map(|t| t.into()).collect()),
             Err(err) => Err(sql_err_to_grpc_error(err)),
         }
     }
 
+    #[instrument]
     pub async fn get_by_id(pool: &PgPool, id: i32) -> Result<UserData, Status> {
         match sqlx::query_as!(Users, "SELECT id, email from users WHERE id = $1", id)
             .fetch_one(pool)
             .await
         {
-            Ok(user) => Ok(user.into_response()),
+            Ok(user) => Ok(user.into()),
             Err(err) => Err(sql_err_to_grpc_error(err)),
         }
     }
 
+    #[instrument]
     pub async fn get_by_email(pool: &PgPool, email: String) -> Result<UserData, Status> {
         match sqlx::query_as!(Users, "SELECT id, email from users WHERE email = $1", email)
             .fetch_one(pool)
             .await
         {
-            Ok(user) => Ok(user.into_response()),
+            Ok(user) => Ok(user.into()),
             Err(err) => Err(sql_err_to_grpc_error(err)),
         }
     }
 
+    #[instrument]
     pub async fn add(pool: &PgPool, email: String) -> Result<UserData, Status> {
         match sqlx::query_as!(
             Users,
@@ -59,11 +82,12 @@ impl Users {
         .fetch_one(pool)
         .await
         {
-            Ok(r) => Ok(r.into_response()),
+            Ok(r) => Ok(r.into()),
             Err(err) => Err(sql_err_to_grpc_error(err)),
         }
     }
 
+    #[instrument]
     pub async fn update(pool: &PgPool, id: i32, email: String) -> Result<UserData, Status> {
         match sqlx::query_as!(
             Users,
@@ -74,35 +98,30 @@ impl Users {
         .fetch_one(pool)
         .await
         {
-            Ok(_) => Ok(Users::default().into_response()),
+            Ok(_) => Ok(Users::default().into()),
             Err(err) => Err(sql_err_to_grpc_error(err)),
         }
     }
 
+    #[instrument]
     pub async fn delete_by_id(pool: &PgPool, id: i32) -> Result<UserData, Status> {
         match sqlx::query_as!(Users, "DELETE FROM users WHERE id = $1", id)
             .fetch_one(pool)
             .await
         {
-            Ok(_) => Ok(Users::default().into_response()),
+            Ok(_) => Ok(Users::default().into()),
             Err(err) => Err(sql_err_to_grpc_error(err)),
         }
     }
 
+    #[instrument]
     pub async fn delete_by_email(pool: &PgPool, email: String) -> Result<UserData, Status> {
         match sqlx::query_as!(Users, "DELETE FROM users WHERE email = $1", email)
             .fetch_one(pool)
             .await
         {
-            Ok(_) => Ok(Users::default().into_response()),
+            Ok(_) => Ok(Users::default().into()),
             Err(err) => Err(sql_err_to_grpc_error(err)),
-        }
-    }
-
-    fn into_response(&self) -> UserData {
-        UserData {
-            id: self.id,
-            email: self.email.clone(),
         }
     }
 }
