a9d09fba6db2b2cd2ba8268b0d3b69b546dbca0a
bhechinger <wonko@4amlunch.net>
2022-11-19 17:05:36 +0000

    Working on getting the handlers going

--- src/storage/users.rs
+++ src/storage/users.rs
@@ -1,33 +1,44 @@
 use diesel::prelude::*;
+// use tracing::{instrument, warn};
 
-use crate::models::users::{IdOrEmail, NewUser, User};
+use crate::models::users::{User, IdOrEmail, NewUser};
 use crate::schema::users::dsl::*;
 
-pub fn create_user(conn: &mut PgConnection, user_email: &str) -> QueryResult<User> {
-    let new_user = NewUser { email: user_email };
-
-    diesel::insert_into(users)
-        .values(&new_user)
-        .get_result(conn)
-}
-
-pub fn get_users(conn: &mut PgConnection) -> QueryResult<Vec<User>> {
-    users.load::<User>(conn)
-}
-
-pub fn get_user(conn: &mut PgConnection, id_or_email: &IdOrEmail) -> QueryResult<Vec<User>> {
-    match id_or_email {
-        IdOrEmail::Id(user_id) => users.find(user_id).load::<User>(conn),
-        IdOrEmail::Email(user_email) => users.filter(email.eq(user_email)).load::<User>(conn),
+impl User {
+    // #[instrument]
+    pub fn all(conn: &mut PgConnection) -> QueryResult<Vec<User>> {
+        users.load::<User>(conn)
     }
-}
 
-pub fn update_user(conn: &mut PgConnection, user_data: User) -> QueryResult<User> {
-    diesel::update(users.find(user_data.id))
-        .set(email.eq(user_data.email))
-        .get_result::<User>(conn)
-}
+    // #[instrument]
+    pub fn get(conn: &mut PgConnection, id_or_email: &IdOrEmail) -> QueryResult<User> {
+        match id_or_email {
+            IdOrEmail::Id(user_id) => users.find(user_id).first::<User>(conn),
+            IdOrEmail::Email(user_email) => users.filter(email.eq(user_email)).first::<User>(conn),
+        }
+    }
 
-pub fn delete_user(conn: &mut PgConnection, user_id: &i32) -> QueryResult<usize> {
-    diesel::delete(users.find(user_id)).execute(conn)
+    // #[instrument]
+    pub async fn add(conn: &mut PgConnection, user_email: &str) -> QueryResult<User> {
+        let new_user = NewUser { email: user_email };
+
+        diesel::insert_into(users)
+            .values(&new_user)
+            .get_result(conn)
+    }
+
+    // #[instrument]
+    pub fn update(conn: &mut PgConnection, user_data: User) -> QueryResult<User> {
+        diesel::update(users.find(user_data.id))
+            .set(email.eq(user_data.email))
+            .get_result::<User>(conn)
+    }
+
+    // #[instrument]
+    pub fn delete(conn: &mut PgConnection, id_or_email: IdOrEmail) -> QueryResult<usize> {
+        match id_or_email {
+            IdOrEmail::Id(user_id) => diesel::delete(users.find(user_id)).execute(conn)
+            IdOrEmail::Email(user_email) => diesel::delete(users.filter(email.eq(user_email))).execute(conn)
+        }
+    }
 }
