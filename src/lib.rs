0580ff612d917031e449b99a67a7d0459e69d30b
bhechinger <wonko@4amlunch.net>
2022-11-19 21:22:28 +0000

    Ok, that didn't work, doing it this way instead

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 pub mod api;
-pub mod models;
 pub mod schema;
 pub mod storage;
 pub mod handlers;
--- src/storage/users.rs
+++ src/storage/users.rs
@@ -1,17 +1,61 @@
 use diesel::prelude::*;
-// use tracing::{instrument, warn};
 
-use crate::models::users::{User, IdOrEmail, NewUser};
+use crate::api::UserData;
+use crate::schema::users;
 use crate::schema::users::dsl::*;
+
+#[derive(Queryable)]
+pub struct User {
+    pub id: i32,
+    pub email: String,
+}
+
+#[derive(Insertable)]
+#[diesel(table_name = users)]
+pub struct NewUser<'a> {
+    pub email: &'a str,
+}
+
+pub enum IdOrEmail {
+    Id(i32),
+    Email(String),
+}
+
+impl Default for User {
+    fn default() -> User {
+        User {
+            id: 0,
+            email: "".to_string(),
+        }
+    }
+}
+
+impl From<User> for UserData {
+    fn from(u: User) -> UserData {
+        UserData {
+            id: u.id,
+            email: u.email,
+        }
+    }
+}
+
+impl From<&User> for UserData {
+    fn from(u: &User) -> UserData {
+        UserData {
+            id: u.id.clone(),
+            email: u.email.clone(),
+        }
+    }
+}
 
 impl User {
     // #[instrument]
-    pub fn all(conn: &mut PgConnection) -> QueryResult<Vec<User>> {
+    pub async fn all(conn: &mut PgConnection) -> QueryResult<Vec<User>> {
         users.load::<User>(conn)
     }
 
     // #[instrument]
-    pub fn get(conn: &mut PgConnection, id_or_email: &IdOrEmail) -> QueryResult<User> {
+    pub async fn get(conn: &mut PgConnection, id_or_email: &IdOrEmail) -> QueryResult<User> {
         match id_or_email {
             IdOrEmail::Id(user_id) => users.find(user_id).first::<User>(conn),
             IdOrEmail::Email(user_email) => users.filter(email.eq(user_email)).first::<User>(conn),
@@ -28,14 +72,14 @@ impl User {
     }
 
     // #[instrument]
-    pub fn update(conn: &mut PgConnection, user_data: User) -> QueryResult<User> {
+    pub async fn update(conn: &mut PgConnection, user_data: User) -> QueryResult<User> {
         diesel::update(users.find(user_data.id))
             .set(email.eq(user_data.email))
             .get_result::<User>(conn)
     }
 
     // #[instrument]
-    pub fn delete(conn: &mut PgConnection, id_or_email: IdOrEmail) -> QueryResult<usize> {
+    pub async fn delete(conn: &mut PgConnection, id_or_email: IdOrEmail) -> QueryResult<usize> {
         match id_or_email {
             IdOrEmail::Id(user_id) => diesel::delete(users.find(user_id)).execute(conn)
             IdOrEmail::Email(user_email) => diesel::delete(users.filter(email.eq(user_email))).execute(conn)
