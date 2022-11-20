d6921642db5c82dfe4bf795b89b654567d56b754
bhechinger <wonko@4amlunch.net>
2022-11-20 17:39:36 +0000

    users now done and server put back into place.

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -3,7 +3,6 @@ use std::env;
 use diesel::prelude::*;
 use diesel::r2d2::ConnectionManager;
 use diesel::r2d2::Pool;
-use diesel::result::Error;
 use dotenvy::dotenv;
 use tonic::transport::Server;
 
@@ -15,10 +14,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt().try_init().unwrap();
     dotenv().ok();
 
-    // let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    let db_url = database_url_for_env();
+    let db_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let db_max_conn_str = env::var("DB_MAX_CONNECTION").unwrap_or_else(|_| "5".to_string());
-    let db_max_conn = db_max_conn_str.parse::<u32>().unwrap()?;
+    let db_max_conn = db_max_conn_str.parse::<u32>().unwrap();
     let grpc_host = env::var("GRPC_HOST").unwrap_or_else(|_| "[::1]".to_string());
     let grpc_port = env::var("GRPC_PORT").unwrap_or_else(|_| "50051".to_string());
 
@@ -29,26 +27,26 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .test_on_check_out(true)
         .max_size(db_max_conn)
         .build(manager)
-        .expect("Could not build connection pool")?;
+        .expect("Could not build connection pool");
 
     let addr = format!("{}:{}", grpc_host, grpc_port).parse()?;
 
-    let agent = agents::AgentService::new(pool.clone());
+    // let agent = agents::AgentService::new(pool.clone());
     // let auth = login::AuthService::new(pool.clone());
     // let router = routers::RouterService::new(pool.clone());
     // let tunnel = tunnels::TunnelService::new(pool.clone());
     let user = users::UserService::new(pool.clone());
-    let permission = permissions::PermissionService::new(pool.clone());
+    // let permission = permissions::PermissionService::new(pool.clone());
 
     println!("Running on port {}", grpc_port);
 
     Server::builder()
-        .add_service(agent_server::AgentServer::new(agent))
+        // .add_service(agent_server::AgentServer::new(agent))
         // .add_service(auth_server::AuthServer::new(auth))
         // .add_service(router_server::RouterServer::new(router))
         // .add_service(tunnel_server::TunnelServer::new(tunnel))
         .add_service(user_server::UserServer::new(user))
-        .add_service(permission_server::PermissionServer::new(permission))
+        // .add_service(permission_server::PermissionServer::new(permission))
         .serve(addr)
         .await?;
 
