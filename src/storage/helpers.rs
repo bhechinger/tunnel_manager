df3a97ef6c3ff7e6111d88e0eb49a98182412f85
bhechinger <wonko@4amlunch.net>
2023-03-10 11:11:11 +0000

    Not sure where I left off

--- src/storage/helpers.rs
+++ src/storage/helpers.rs
@@ -1,9 +1,17 @@
 use diesel::result::Error;
 use tonic::Status;
+use bcrypt::BcryptError;
 
 pub fn sql_err_to_grpc_error(error: Error) -> Status {
     match error {
         Error::NotFound => Status::not_found("not found".to_string()),
+        _ => Status::internal(error.to_string()),
+    }
+}
+
+pub fn bcrypt_err_to_grpc_error(error: BcryptError) -> Status {
+    match error {
+        BcryptError::InvalidHash(_) => Status::permission_denied("invalid password".to_string()),
         _ => Status::internal(error.to_string()),
     }
 }
